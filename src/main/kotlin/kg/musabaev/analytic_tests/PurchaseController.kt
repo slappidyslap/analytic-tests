316504512b114494814a6dbf114ca3dce6e770bb
Eld <musabaeveldiar@gmail.com>
2025-03-25 12:16:54 +0600

    add Measurement Protocol GA4 API Key to external config file

--- src/main/kotlin/kg/musabaev/analytic_tests/PurchaseController.kt
+++ src/main/kotlin/kg/musabaev/analytic_tests/PurchaseController.kt
@@ -1,16 +1,23 @@
 package kg.musabaev.analytic_tests
 
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class PurchaseController {
+
     private val log = LoggerFactory.getLogger(PurchaseController::class.java)
 
+    @Value("\${MP_GA4_API_KEY}")
+    private lateinit var mpga4ApiKey: String
+
     @GetMapping("/purchase-flow")
-    fun purchaseFlowPage(): String {
+    fun purchaseFlowPage(model: Model): String {
         log.info("PurchaseFlow")
+        model.addAttribute("mpga4ApiKey", mpga4ApiKey.substring(7))
         return "purchase-flow"
     }
 }
